48a9f9b87358b410d9d715cbeb0fe5d159f25921
muriloebsantos <murilo.bacagini@gmail.com>
2022-07-14 10:25:25 -0300

    Track User Activity

--- src/app/core/services/@base/http.service.ts
+++ src/app/core/services/@base/http.service.ts
@@ -11,12 +11,14 @@ export class HttpService {
     }
 
     protected getAuthenticated<T>(path: string) : Observable<T> {
+        this.updateUserActivity();
         return this.httpClient.get<T>(
             environment.api + path, this.getAuthOptions()
         );
     }
 
     protected postAuthenticated<T>(path: string, body: any) : Observable<T> {
+        this.updateUserActivity();
         return this.httpClient.post<T>(
             environment.api + path, body, this.getAuthOptions()
         );
@@ -29,15 +31,23 @@ export class HttpService {
     }
 
     protected putAuthenticated<T>(path: string, body: any) : Observable<T> {
+        this.updateUserActivity();
         return this.httpClient.put<T>(
             environment.api + path, body, this.getAuthOptions()
         );
     }
 
     protected deleteAuthenticated<T>(path: string) : Observable<T> {
+        this.updateUserActivity();
         return this.httpClient.delete<T>(
             environment.api + path, this.getAuthOptions()
         );
+    }
+
+    private updateUserActivity() {
+        this.httpClient.post(
+            environment.api + "/me/activity", null, this.getAuthOptions()
+        ).subscribe();
     }
 
     private getAuthOptions () {
